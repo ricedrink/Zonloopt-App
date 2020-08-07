ae1eb1a6f3bc33f504273723f0991ed12f1a1324
ricedrink <alicezhang32092@gmail.com>
2020-08-06 20:11:57 -0400

    added MapKit!

--- zonloopt/HomeViewController.swift
+++ zonloopt/HomeViewController.swift
@@ -7,8 +7,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+
+    let manager = CLLocationManager()
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+       }
+       
+       override func viewDidAppear(_ animated: Bool) {
+           super.viewDidAppear(animated)
+           manager.desiredAccuracy = kCLLocationAccuracyBest
+           manager.delegate = self
+           manager.requestWhenInUseAuthorization()
+           manager.startUpdatingLocation()
+       }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation){
+        let coordinate = CLLocationCoordinate2DMake(location.coordinate.latitude,
+                                                    location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
     
     @IBAction func logOutAction(_ sender: UIButton) {
         //send user back
@@ -18,11 +53,6 @@ class HomeViewController: UIViewController {
         present(vc, animated: true)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
 
     /*
